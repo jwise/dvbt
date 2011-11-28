5fa4dc08a790b4c422099e7472941a8f797736f5
Joshua Wise <joshua@joshuawise.com>
2011-11-27 19:51:30 -0800

    ofdmvis: Integrate estimator.

--- ofdmvis.c
+++ ofdmvis.c
@@ -13,6 +13,10 @@ typedef struct ofdm_state {
 	/* Parameters */
 	int fft_size; /* FFT size */
 	int guard_len; /* guard length */
+	/* Changing either of these parameters requires a cleanup and
+	 * resynchronization.  */
+	 
+	double snr;
 	
 	/* Sample receiver */
 	int cursamp;
@@ -20,6 +24,9 @@ typedef struct ofdm_state {
 	double *samples;
 	
 	/* Estimator */
+	fftw_complex *estim_buf;
+	int estim_refill; /* How many samples we have stored already. */
+	double complex estim_phase;
 	
 	/* FFT */
 	fftw_plan fft_plan;
@@ -80,16 +87,95 @@ void ofdm_getsamples(ofdm_state_t *ofdm, int nreq, fftw_complex *out)
 	}
 }
 
+/* Symbol estimation, as per [Beek97]. */
+
 void ofdm_estimate_symbol(ofdm_state_t *ofdm, fftw_complex *sym)
 {
-	fftw_complex *p = alloca(ofdm->guard_len * sizeof(fftw_complex));
+	int N = ofdm->fft_size;
+	int L = ofdm->guard_len;
+	double rho = ofdm->snr / (ofdm->snr + 1.0);
 
-	/* Do the simplest possible thing to begin with. */
-	ofdm_getsamples(ofdm, ofdm->fft_size, sym);
-	ofdm_getsamples(ofdm, ofdm->guard_len, p);
+	if (!ofdm->estim_buf)
+		ofdm->estim_buf = fftw_malloc(sizeof(fftw_complex) * (2*N + L));
+	
+	/* Ideally, we'd like to maintain the buffer looking like this:
+	 *
+	 *   +-------+-----------------------+-------+----------X
+	 *   | G_n-1 |   S Y M B O L   n+0   | G_n+0 |   S Y M B O L  n+1
+	 *   +-------+-----------------------+-------+----------X
+	 *
+	 * The algorithm is most effective at aligning the symbol when the
+	 * symbol is in the middle of the 2N+L block.  So, we'll keep it
+	 * there.  The paper suggests using the time calibration only for
+	 * acquisition; we'll see how the jitter works out here.
+	 *
+	 * We always consume into the output starting from where the
+	 * algorithm recommended.  We then shift back in the buffer starting
+	 * at ofs + N, so that what we currently call G_n+0 will be at the
+	 * beginning of the buffer for next time.
+	 */
+	
+	ofdm_getsamples(ofdm, 2*N + L - ofdm->estim_refill, ofdm->estim_buf + ofdm->estim_refill);
 
-#warning XXX: estimate_symbol doesn't
-}
+	/* Prime the running sums. */
+	double complex gam = 0, Phi = 0;
+	int k;
+#define C(p) (ofdm->estim_buf[p][0] + ofdm->estim_buf[p][1] * 1.0i)
+#define GAM(n) C(n) * conj(C(n+N))
+#define PHI(n) 0.5 * (pow(cabs(C(n)), 2.0) + pow(cabs(C(n+N)), 2.0))
+	for (k = 0; k < L; k++)
+	{
+		gam += GAM(k);
+		Phi += PHI(k);
+	}
+	
+	/* argmax(0 >= i > 2N, cabs(Gam(i)) - rho * Phi(i)) */
+	double max = -INFINITY;
+	double complex bestgam = 1.0;
+	int argmax = 0;
+	for (k = 0; k < N+L; k++)
+	{
+		double n = cabs(gam) - rho * Phi;
+		if (n > max)
+		{
+			max = n;
+			bestgam = gam;
+			argmax = k;
+		}
+		
+		gam -= GAM(k);
+		Phi -= PHI(k);
+		if (k != (2*N-1))
+		{
+			gam += GAM(k+L);
+			Phi += PHI(k+L);
+		}
+	}
+	
+	/* Now we have an estimation at argmax. */
+	double epsilon = (-1.0 / (2.0 * M_PI)) * carg(bestgam);
+	
+	for (k = 0; k < N; k++)
+	{
+		double complex c;
+		
+		c = C(k + L + argmax);
+		ofdm->estim_phase += 2.0i * M_PI * ((epsilon + .05 /* ??? */) / (double)N);
+		c *= cexp(ofdm->estim_phase);
+		
+		sym[k][0] = creal(c);
+		sym[k][1] = cimag(c);
+	}
+	
+	ofdm->estim_refill = 2*N + L - (argmax + N);
+	memmove(ofdm->estim_buf, ofdm->estim_buf + argmax + N, sizeof(fftw_complex) * ofdm->estim_refill);
+
+#undef GAM
+#undef PHI
+#undef C	
+	
+	printf("adj: %d %f\n", argmax, epsilon);
+}	
 
 uint32_t hsvtorgb(float H, float S, float V)
 {
@@ -170,8 +256,6 @@ void ofdm_fft_symbol(ofdm_state_t *ofdm)
 	if (!ofdm->fft_out)
 		ofdm->fft_out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * ofdm->fft_size);
 	assert(ofdm->fft_out);
-	
-	/* Why MEASURE?  Well, fftw wisdom will save us in the long term. */
 	if (!ofdm->fft_plan)
 		ofdm->fft_plan = fftw_plan_dft_1d(ofdm->fft_size, ofdm->fft_in, ofdm->fft_out, FFTW_FORWARD, FFTW_MEASURE);
 	assert(ofdm->fft_plan);
@@ -236,11 +320,14 @@ int main(int argc, char** argv)
 {
 	SDL_Event ev;
 	int guard_ofs = 0;
-	int carrier = 853;
 	float frqshift = 0;
+	
+	memset(&ofdm, 0, sizeof(ofdm));
 	
 	ofdm.fft_size = 2048;
 	ofdm.guard_len = ofdm.fft_size / 32;
+	ofdm.fft_dbg_carrier = 853;
+	ofdm.snr = 100.0; /* 20dB */
 	
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0)
 	{
@@ -278,12 +365,12 @@ int main(int argc, char** argv)
 			    ev.key.keysym.sym == SDLK_q)
 			    	exit(0);
 			else if (ev.key.keysym.sym == SDLK_LEFT) {
-				carrier--;
-				printf("Viewing carrier %d\n", carrier);
+				ofdm.fft_dbg_carrier--;
+				printf("Viewing carrier %d\n", ofdm.fft_dbg_carrier);
 				need_rerender = 1;
 			} else if (ev.key.keysym.sym == SDLK_RIGHT) {
-				carrier++;
-				printf("Viewing carrier %d\n", carrier);
+				ofdm.fft_dbg_carrier++;
+				printf("Viewing carrier %d\n", ofdm.fft_dbg_carrier);
 				need_rerender = 1;
 			} else if (ev.key.keysym.sym == SDLK_g) {
 				if (ev.key.keysym.mod & KMOD_SHIFT)
