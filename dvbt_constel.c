0002e0887b3f8342c49f5c1aa655147896eeb980
Joshua Wise <joshua@joshuawise.com>
2020-07-02 16:26:53 -0400

    ofdm_constel: fix symbol deinterleaver; add bitwise deinterleaver and demux

--- dvbt_constel.c
+++ dvbt_constel.c
@@ -10,7 +10,7 @@ void ofdm_constel(ofdm_state_t *ofdm)
 		return;
 	}
 	
-	if (!ofdm->constel_ready && ofdm->symbol != 0) {
+	if (!ofdm->constel_ready && (ofdm->symbol != 0 || ofdm->frame != 0)) {
 		LOUD("constel: waiting for symbol sync\n");
 		return;
 	}
@@ -108,13 +108,47 @@ void ofdm_constel(ofdm_state_t *ofdm)
 		printf("constel: unpacked wrong number of Ys %d sym %d should be %d, tps_c %d, pilot_c %d\n", yptr, ofdm->symbol, ofdm->fft->n_max, tps_c, pilot_c);
 	}
 	
+	if (ofdm->symbol == 0 && ofdm->frame == 0) {
+		printf("ys[0] = %x, 1024 = %x, 16 = %x\n", ys[0], ys[1024], ys[16]);
+	}
+	
 	/* section 4.3.4.2: symbol deinterleaver */
 	uint8_t yps[6048];
 	for (c = 0; c <= ofdm->fft->k_max; c++) {
 		if ((ofdm->symbol % 2) == 0) {
 			yps[c] = ys[ofdm->fft->scram_h[c]];
 		} else {
-			yps[ofdm->fft->scram_h[c]] = yps[c];
+			yps[ofdm->fft->scram_h[c]] = ys[c];
 		}
 	}
+	
+	/* bitwise interleaver output a_n,w is defined in terms of y'_w */
+	const int Hk[] = {0, 63, 105, 42, 21, 84};
+#define a(n,w) ((yps[w] >> (ybits - (n) - 1)) & 1) /* symbol interleaver, figure 7a */
+#define b(e,w) (a(e, ((w) + 126 - Hk[e]) % 126 + (w) / 126 * 126)) /* bit interleaver, figure 7a */
+
+	/* demux from b[x,y] to x */
+	uint8_t xs[6048 / 8] = {}; /* note that first bit in bit-serial order is bit 7!  i.e., x = {xs[0][7:0], xs[1][7:0], ...} */
+	int bit = 0;
+#define PUTBIT(b) do { \
+		xs[bit / 8] |= (b) << (7 - (bit % 8)); \
+		bit++; \
+	} while(0)
+	
+	for (c = 0; c < ofdm->fft->n_max; c++) {
+		switch (ofdm->tps_constellation) {
+		case CONSTEL_QAM16:
+			PUTBIT(b(0, c));
+			PUTBIT(b(2, c));
+			PUTBIT(b(1, c));
+			PUTBIT(b(3, c));
+			break;
+		default:
+			printf("constel: bad constellation %d\n", ofdm->tps_constellation);
+			return;
+		}
+	}
+	
+	printf("constel: deinterleaved %d bits, xs[0] = %02x\n", bit, xs[0]);
+	write(2, xs, bit/8);
 }
